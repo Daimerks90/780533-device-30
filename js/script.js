42ad2ebdaad5abaf5ff08afc4544f8ed77ffe8a0
Daimerks90 <daimerks90@gmail.com>
2021-03-15 17:50:22 +0300

    Исправления для защиты

--- js/script.js
+++ js/script.js
@@ -26,13 +26,48 @@ try {
   isStorageSupport = false;
 }
 
-writeForm.addEventListener('submit', () =>{
-  if (isStorageSupport) {
-    storageName = localStorage.setItem('name', inputModalName.value);
-    storageEmail = localStorage.setItem('email', inputModalEmail.value);
-    storageMassage = localStorage.setItem('message', inputModalMessage.value);
+writeForm.addEventListener('submit', (evt) => {
+  const validName = inputModalName.value.length;
+  const validEmail = inputModalEmail.value.length;
+  const validMessage = inputModalMessage.value.length;
+  if ((validName < 3 || validEmail < 3 || validMessage < 3)) {
+    evt.preventDefault();
+    if (validName < 3) {
+      inputModalName.classList.add('write__input--invalid');
+    }
+    if (validEmail < 3) {
+      inputModalEmail.classList.add('write__input--invalid');
+    }
+    if (validMessage < 3) {
+      inputModalMessage.classList.add('write__text--invalid');
+    }
+  } else {
+    if (isStorageSupport) {
+      storageName = localStorage.setItem('name', inputModalName.value);
+      storageEmail = localStorage.setItem('email', inputModalEmail.value);
+      storageMassage = localStorage.setItem('message', inputModalMessage.value);
+    }
   }
-})
+});
+
+inputModalName.addEventListener('input', () => {
+  if (inputModalName.value.length > 3) {
+    inputModalName.classList.remove('write__input--invalid');
+  }
+});
+
+inputModalMessage.addEventListener('input', () => {
+  if (inputModalMessage.value.length > 3) {
+    inputModalMessage.classList.remove('write__input--invalid');
+  }
+});
+
+inputModalEmail.addEventListener('input', () => {
+  if (inputModalEmail.value.length > 3) {
+    inputModalEmail.classList.remove('write__text--invalid');
+  }
+});
+
 
 for (let i = 0; i < slidesButtons.length; i += 1) {
   slidesButtons[i].addEventListener('click', () => {
